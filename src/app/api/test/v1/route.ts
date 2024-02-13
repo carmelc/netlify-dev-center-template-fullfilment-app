585c9e320356633f93bfb211f76bba40c645d6be
carmelc <carmelc@wix.com>
2024-02-13 16:07:38 +0200

    Test netlify routes

--- src/app/api/test/v1/route.ts
+++ src/app/api/test/v1/route.ts
@@ -1,11 +1,12 @@
-import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 
-export async function GET(request: Request, response: NextResponse) {
+export async function GET(request: NextRequest) {
   console.info('Test Me::POST - called');
   const headers = request.headers;
 
   return Response.json({
     request_url: request.url,
+    next_request_url: request.nextUrl.href,
     'x-middleware-request-url': headers.get('x-middleware-request-url'),
     'x-middleware-request-legacy-url': headers.get('x-middleware-request-legacy-url'),
   });
